364a432dc837505cf3fec2a40f236a0042c3d889
Ofek Segman <ofeksegman@gmail.com>
2025-01-19 15:29:39 +0200

    recycle posts

--- app/src/main/java/com/example/ease/FeedFragment.kt
+++ app/src/main/java/com/example/ease/FeedFragment.kt
@@ -9,20 +9,23 @@ import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ease.model.Model
+import com.example.ease.model.Post
 
 class PostsViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
-    var profileNameTextView: TextView?=null
-    var postTextView : TextView?=null
-    var imageProfile: ImageView?=null
+    var profileNameTextView: TextView? = null
+    var postTextView: TextView? = null
+    var imageProfile: ImageView? = null
 
 
     init {
-        profileNameTextView=itemView.findViewById(R.id.profileName)
-        postTextView=itemView.findViewById(R.id.textPost)
+        profileNameTextView = itemView.findViewById(R.id.profileName)
+        postTextView = itemView.findViewById(R.id.textPost)
 
     }
-       /*itemView.setOnClickListener{
+    /*itemView.setOnClickListener{
             adapterPosition
         }
         itemView.findViewById<View>(R.id.student_row).setOnClickListener {
@@ -38,20 +41,55 @@ class PostsViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
         }
 */
 
+
+    fun bind(post: Post) {
+        profileNameTextView?.text = post.profileName
+        postTextView?.text = post.textPost
+    }
+
+}
+
+class PostRecycleAdapter(private val posts : List<Post>?): RecyclerView.Adapter<PostsViewHolder>() {
+    override fun getItemCount(): Int {
+        return posts?.size ?: 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostsViewHolder {
+        val inflation=LayoutInflater.from(parent.context)
+        val view = inflation.inflate(R.layout.post_row, parent, false)
+        return PostsViewHolder(view);
     }
 
 
+    override fun onBindViewHolder(holder: PostsViewHolder, position: Int) {
+        holder.bind(posts?.get(position) ?: return)
+    }
+
+
+}
+
+
 class FeedFragment : Fragment() {
-
-
+    //var adapter: PostRecycleAdapter? = null
+    var adapter = PostRecycleAdapter(Model.shared.posts)
+    var posts: MutableList<Post> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_feed, container, false)
+        var view = inflater.inflate(R.layout.fragment_feed, container, false)
+
+        posts = Model.shared.posts
+        val recyclerView: RecyclerView = view.findViewById(R.id.fragment_feed_recycler_view)
+        recyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = PostRecycleAdapter(posts)
+        recyclerView.adapter = adapter
+        return view
     }
-
-
 }
